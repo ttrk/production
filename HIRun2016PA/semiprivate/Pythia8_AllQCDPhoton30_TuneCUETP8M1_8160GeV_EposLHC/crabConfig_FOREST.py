669508bd21508aafcceafd3aeabbe92fc957ddd7
Kaya Tatar <tatark@mit.edu>
2016-11-02 22:58:06 +0100

    foresting configurations for semiprivate samples : AllQCDPhoton15 AllQCDPhoton30
    Samples are listed on Twiki
    https://twiki.cern.ch/twiki/bin/view/CMS/PPb8TeVPrivateMC?rev=11#cmsDriver_commands

---
+++ HIRun2016PA/semiprivate/Pythia8_AllQCDPhoton30_TuneCUETP8M1_8160GeV_EposLHC/crabConfig_FOREST.py
@@ -0,0 +1,33 @@
+from WMCore.Configuration import Configuration
+
+config = Configuration()
+
+config.section_("General")
+config.General.requestName = "Pythia8_AllQCDPhoton30_EposLHC_FOREST"
+config.General.transferLogs = False
+
+config.section_("JobType")
+config.JobType.pluginName = "Analysis"
+config.JobType.psetName = "runForestAOD_pPb_MC_80X.py"
+# config.JobType.maxMemoryMB = 3500
+# forest_CMSSW_8_0_22
+# https://github.com/CmsHI/cmssw/commit/a796484d9fb6a6266dc5c482615c1cc1c5763223
+# runForestAOD_pPb_MC_80X.py with commit
+# https://github.com/CmsHI/cmssw/commit/8eb76699d997f6bb8e1b85891417b3c53a537d7d
+# related : https://hypernews.cern.ch/HyperNews/CMS/get/hi-general/3409.html
+
+config.section_("Data")
+config.Data.inputDataset = "/AllQCDPhoton30_GEN_SIM/gsfs-AllQCDPhoton30_RECO25ns_10252016-8903f9f070d6a6d315ca9abbd5d692c3/USER"
+config.Data.inputDBS = "phys03"
+config.Data.splitting = "FileBased"
+config.Data.unitsPerJob = 2
+config.Data.totalUnits = -1
+config.Data.publication = False
+config.Data.outputDatasetTag = "Pythia8_AllQCDPhoton30_EposLHC_FOREST"
+config.Data.outLFNDirBase = "/store/user/katatar/HIRun2016PA/semiprivate/"
+
+config.section_("Site")
+config.Site.storageSite = "T2_US_MIT"
+config.Site.whitelist = ["T2_US_MIT"]
+# config.Debug.extraJDL = ["+CMS_ALLOW_OVERFLOW=False"]
+
