e68fc7f4044f5d99e6fd58406003acee205f42b7
Kaya Tatar <tatark@mit.edu>
2017-09-11 17:55:31 -0400

    configuration to forest HIMinimumBias2 data sample for run range : 263349-263379

---
+++ data/HIMinimumBias2_Run263349_263379/crabConfig.py
@@ -0,0 +1,37 @@
+from WMCore.Configuration import Configuration
+
+config = Configuration()
+
+config.section_('General')
+config.General.requestName = 'HIMinimumBias2_Run263349_263379'
+config.General.transferLogs = True
+
+config.section_('JobType')
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'runForestAOD_PbPb_DATA_75X.py'
+config.JobType.maxMemoryMB = 2500    # request high memory machines, 2500 is the maximum guaranteed number.
+config.JobType.maxJobRuntimeMin = 2800    # request longer runtime, ~47 hours. 2800 is the maximum guaranteed number.
+# https://github.com/CmsHI/cmssw/commit/c32f0fc41c29226dc80cac56a4ef063f4ea4b2df
+# runForestAOD_PbPb_DATA_75X.py commit
+# https://github.com/CmsHI/cmssw/commit/c32f0fc41c29226dc80cac56a4ef063f4ea4b2df
+# relevant DAS query : file run between [263349, 263379] dataset=/HIMinimumBias2/HIRun2015-PromptReco-v1/AOD | sum(file.nevents)
+
+config.section_('Data')
+config.Data.inputDataset = '/HIMinimumBias2/HIRun2015-PromptReco-v1/AOD'
+config.Data.inputDBS = 'global'
+config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/HI/Cert_262548-263757_PromptReco_HICollisions15_JSON_v2.txt'
+config.Data.runRange = '263349-263379'
+config.Data.splitting = "FileBased"
+config.Data.unitsPerJob = 1
+config.Data.totalUnits = -1
+config.Data.publication = False
+config.Data.outputDatasetTag = 'HIRun2015-PromptReco-v1-Run263349-263379'
+config.Data.outLFNDirBase = '/store/user/katatar/'
+
+# https://github.com/richard-cms/production/commits/2016-03-17-reRECO-foresting/crabConfig.py
+config.section_('Site')
+config.Site.storageSite = "T2_US_MIT"
+config.Site.whitelist = ["T2_US_Vanderbilt"]
+
+config.section_("Debug")
+config.Debug.extraJDL = ["+CMS_ALLOW_OVERFLOW=False"]
