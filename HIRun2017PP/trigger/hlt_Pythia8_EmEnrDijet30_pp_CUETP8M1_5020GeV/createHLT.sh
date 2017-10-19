d1703ddecc4278cc3f830c9ac306832de13e1c7d
Kaya Tatar <tatark@mit.edu>
2017-10-19 09:55:11 -0400

    HLT configurations to run over Pythia8 EmEnrDijet30 sample at 5.02 TeV, to be used for 2017 pp reference run preparations
    menu : /users/katatar/HI2017PPRef/HIHighPtRefPP5TeVPhoton/V2
    related : https://twiki.cern.ch/twiki/bin/view/CMS/HiHighPtTrigger2017?rev=12
    
    Samples are listed on Twiki
    https://twiki.cern.ch/twiki/bin/view/CMS/PP5TeV2017PrivateMC?rev=12#Available_Semi_Private_Samples

---
+++ HIRun2017PP/trigger/hlt_Pythia8_EmEnrDijet30_pp_CUETP8M1_5020GeV/createHLT.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+hltGetConfiguration /users/katatar/HI2017PPRef/HIHighPtRefPP5TeVPhoton/V2  --full --offline --mc --unprescale --process TEST  --l1-emulator FullMC --l1Xml L1Menu_Collisions2017_dev_r9_HIppRefMOD.xml  --globaltag 92X_upgrade2017_realistic_v11 --output none --max-events 10 --input root://cms-xrd-global.cern.ch//store/user/gsfs/Pythia8_Photon300_pp_CUETP8M1_5020GeV/RAW_20171002/171002_194612/0000/step2_pp_DIGI_L1_DIGI2RAW_HLT_1.root --setup /dev/CMSSW_9_2_0/GRun > hlt.py
+
+echo '' >> hlt.py
+echo 'process.load("HLTrigger.HLTanalyzers.HLTBitAnalyser_cfi")' >> hlt.py
+echo 'process.hltbitanalysis.HLTProcessName = cms.string("TEST")' >> hlt.py
+echo 'process.hltbitanalysis.hltresults = cms.InputTag( "TriggerResults","","TEST" )' >> hlt.py
+echo 'process.hltbitanalysis.l1GtReadoutRecord = cms.InputTag("simGtDigis","","TEST")' >> hlt.py
+echo 'process.hltbitanalysis.l1GctHFBitCounts = cms.InputTag("gctDigis","","TEST")' >> hlt.py
+echo 'process.hltbitanalysis.l1GctHFRingSums = cms.InputTag("gctDigis","","TEST")' >> hlt.py
+echo 'process.hltbitanalysis.UseTFileService = cms.untracked.bool(True)' >> hlt.py
+echo 'process.hltBitAnalysis = cms.EndPath(process.hltbitanalysis)' >> hlt.py
+echo 'process.TFileService = cms.Service("TFileService",' >> hlt.py
+echo '                                  fileName=cms.string("openHLT.root"))' >> hlt.py
