3ba207f8063923669360a23c1b9d4575b2e90b29
Kaya Tatar <tatark@mit.edu>
2017-11-01 17:10:13 -0400

    update HLT configurations to run over Pythia8 AllQCDPhoton30 sample at 5.02 TeV, to be used for 2017 pp reference run preparations
    menu : /users/cfmcginn/RefPP5TeV2017/PPRef2017MasterCopy/V6
    related : https://twiki.cern.ch/twiki/bin/view/CMS/HiHighPtTrigger2017?rev=12
    
    Samples are listed on Twiki
    https://twiki.cern.ch/twiki/bin/view/CMS/PP5TeV2017PrivateMC?rev=13#Available_Semi_Private_Samples

--- HIRun2017PP/trigger/hlt_Pythia8_Photon30_pp_CUETP8M1_5020GeV/crabConfig.py
+++ HIRun2017PP/trigger/hlt_Pythia8_Photon30_pp_CUETP8M1_5020GeV/crabConfig.py
@@ -3,7 +3,7 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = "hlt_Pythia8_Photon30_pp_CUETP8M1_5020GeV_HIHighPtRefPP5TeVPhoton_V2"
+config.General.requestName = "hlt_Pythia8_Photon30_pp_CUETP8M1_5020GeV_RefPP5TeV2017_V6_fgBitOff"
 config.General.transferLogs = False
 config.General.transferOutputs = True
 
@@ -12,9 +12,12 @@ config.JobType.pluginName = "Analysis"
 config.JobType.psetName = "hlt.py"
 config.JobType.maxMemoryMB = 2500    # request high memory machines, 2500 is the maximum guaranteed number.
 config.JobType.maxJobRuntimeMin = 2800 # request longer runtime, ~47 hours. 2800 is the maximum guaranteed number.
-# CMSSW_9_2_12
-# HLT instructions : https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuideGlobalHLT?rev=3071#Running_the_HLT_with_CMSSW_9_2_0
+# CMSSW_9_2_12_patch1
+# HLT instructions : https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuideGlobalHLT?rev=3077#Running_the_HLT_with_CMSSW_9_2_X
 # hlt.py is the result of ./createHLT.sh + turn off process.dqmOutput
+# turn off some L1 EG bits.
+# disable the fgBit (set to 1) : https://github.com/cms-sw/cmssw/blob/CMSSW_9_2_X/L1Trigger/L1TCalorimeter/src/firmware/Stage2Layer2EGammaAlgorithmFirmwareImp1.cc#L231
+# disable the shapeBit (set to 1) : https://github.com/cms-sw/cmssw/blob/CMSSW_9_2_X/L1Trigger/L1TCalorimeter/src/firmware/Stage2Layer2EGammaAlgorithmFirmwareImp1.cc#L233
 
 config.section_("Data")
 config.Data.inputDataset = "/Pythia8_Photon300_pp_CUETP8M1_5020GeV/gsfs-RAW_20171002-f5c51db4d3712eae044fcbfa9ecb19c0/USER"
@@ -25,7 +28,7 @@ config.Data.totalUnits = -1
 config.Data.publication = False
 config.JobType.outputFiles = ["openHLT.root"]
 config.JobType.numCores = 4
-config.Data.outputDatasetTag = "hlt_HIHighPtRefPP5TeVPhoton_V2"
+config.Data.outputDatasetTag = "hlt_RefPP5TeV2017_V6_fgBitOff"
 config.Data.outLFNDirBase = "/store/user/katatar/HIRun2017PP/"
 
 config.section_("Site")
--- HIRun2017PP/trigger/hlt_Pythia8_Photon30_pp_CUETP8M1_5020GeV/createHLT.sh
+++ HIRun2017PP/trigger/hlt_Pythia8_Photon30_pp_CUETP8M1_5020GeV/createHLT.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-hltGetConfiguration /users/katatar/HI2017PPRef/HIHighPtRefPP5TeVPhoton/V2  --full --offline --mc --unprescale --process TEST  --l1-emulator FullMC --l1Xml L1Menu_Collisions2017_dev_r9_HIppRefMOD.xml  --globaltag 92X_upgrade2017_realistic_v11 --output none --max-events 10 --input root://cms-xrd-global.cern.ch//store/user/gsfs/Pythia8_Photon300_pp_CUETP8M1_5020GeV/RAW_20171002/171002_194612/0000/step2_pp_DIGI_L1_DIGI2RAW_HLT_1.root --setup /dev/CMSSW_9_2_0/GRun > hlt.py
+hltGetConfiguration /users/cfmcginn/RefPP5TeV2017/PPRef2017MasterCopy/V6  --full --offline --mc --unprescale --process TEST  --l1-emulator FullMC --l1Xml L1Menu_Collisions2017_dev_r9_HIppRefMODv3_20171028.xml  --globaltag 92X_upgrade2017_realistic_v11 --output none --max-events 10 --input root://cms-xrd-global.cern.ch//store/user/gsfs/Pythia8_Photon300_pp_CUETP8M1_5020GeV/RAW_20171002/171002_194612/0000/step2_pp_DIGI_L1_DIGI2RAW_HLT_1.root --setup /dev/CMSSW_9_2_0/GRun > hlt.py
 
 echo '' >> hlt.py
+echo 'process.caloStage2Params.egHOverEcutBarrel = cms.int32(1)' >> hlt.py
+echo 'process.caloStage2Params.egHOverEcutEndcap = cms.int32(1)' >> hlt.py
+
 echo 'process.load("HLTrigger.HLTanalyzers.HLTBitAnalyser_cfi")' >> hlt.py
 echo 'process.hltbitanalysis.HLTProcessName = cms.string("TEST")' >> hlt.py
 echo 'process.hltbitanalysis.hltresults = cms.InputTag( "TriggerResults","","TEST" )' >> hlt.py
