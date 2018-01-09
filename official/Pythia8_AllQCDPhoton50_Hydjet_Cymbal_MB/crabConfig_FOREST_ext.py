2316b9abab7c3d44c090c0b844bd33125dcf1528
Kaya Tatar <tatark@mit.edu>
2018-01-09 22:15:14 +0300

    add foresting configurations for extended official Pythia8 AllQCDPhoton Hydjet Cymbal MB samples. Samples are listed on Twiki : https://twiki.cern.ch/twiki/bin/view/CMS/PbPb5TeVOfficialMC?rev=206#Available_Samples

---
+++ official/Pythia8_AllQCDPhoton50_Hydjet_Cymbal_MB/crabConfig_FOREST_ext.py
@@ -0,0 +1,36 @@
+from WMCore.Configuration import Configuration
+
+config = Configuration()
+
+config.section_("General")
+config.General.requestName = "Pythia8_AllQCDPhoton50_Hydjet_Cymbal_MB_FOREST_ext"
+config.General.transferLogs = False
+
+config.section_("JobType")
+config.JobType.pluginName = "Analysis"
+config.JobType.psetName = "runForestAOD_PbPb_MIX_75X.py"
+# config.JobType.maxMemoryMB = 3500
+# forest_CMSSW_7_5_8_patch3
+# https://github.com/CmsHI/cmssw/commit/f61094f7a81de65dc1f9796236b8ed94fca888a6
+# runForestAOD_PbPb_MIX_75X.py commit + change gen particle pt and eta cuts
+# https://github.com/CmsHI/cmssw/commit/336108879815bf68a856f23a7a47254ef87347fd
+# related : 
+# https://hypernews.cern.ch/HyperNews/CMS/get/hi-general/4611.html
+# https://twiki.cern.ch/twiki/bin/view/CMS/PbPb5TeVOfficialMC?rev=206#Available_Samples
+
+config.section_("Data")
+config.Data.inputDataset = "/Pythia8_AllQCDPhoton50_Hydjet_Cymbal_MB/HINPbPbWinter16DR-75X_mcRun2_HeavyIon_v14_ext1-v1/AODSIM"
+config.Data.inputDBS = "global"
+config.Data.splitting = "FileBased"
+config.Data.unitsPerJob = 1
+config.Data.totalUnits = -1
+config.Data.publication = False
+config.Data.outputDatasetTag = "HINPbPbWinter16DR-75X_mcRun2_HeavyIon_v14_ext1-v1-FOREST"
+config.Data.outLFNDirBase = "/store/user/katatar/official/"
+
+config.section_("Site")
+config.Site.storageSite = "T2_US_MIT"
+config.Site.whitelist = ["T2_US_MIT"]
+
+config.section_("Debug")
+config.Debug.extraJDL = ["+CMS_ALLOW_OVERFLOW=False"]
