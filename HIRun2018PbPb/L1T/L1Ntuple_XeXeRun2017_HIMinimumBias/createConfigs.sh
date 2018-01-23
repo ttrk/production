f322830464c533ae15b8c82f000c27fe18c5ba18
Kaya Tatar <tatark@mit.edu>
2018-01-23 11:32:41 -0500

    add configurations to run L1NTuple on XeXeRun2017 HIMinimumBias data sample. This is a starting workflow for HIRun2018 L1Trigger preparations.

---
+++ HIRun2018PbPb/L1T/L1Ntuple_XeXeRun2017_HIMinimumBias/createConfigs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+runCmd="/afs/cern.ch/user/k/katatar/code/scripts/myRun.sh"
+
+inputFile="root://xrootd.cmsaf.mit.edu//store/hidata/XeXeRun2017/HIMinimumBias8/RAW/v1/000/304/899/00000/02339E6D-94AF-E711-A9FC-02163E0122EE.root"
+#inputFile="root://xrootd.cmsaf.mit.edu//store/hidata/XeXeRun2017/HIMinimumBias8/RAW/v1/000/304/875/00000/CA409ED8-58AF-E711-9918-02163E01A3EA.root"
+#inputFile="file:CA409ED8-58AF-E711-9918-02163E01A3EA.root"
+
+$runCmd cmsDriver.py l1Ntuple -s RAW2DIGI --era=Run2_2017_pp_on_XeXe --customise=L1Trigger/Configuration/customiseReEmul.L1TReEmulFromRAW --customise=L1Trigger/L1TNtuples/customiseL1Ntuple.L1NtupleEMU --customise=L1Trigger/Configuration/customiseUtils.L1TTurnOffUnpackStage2GtGmtAndCalo --conditions=auto:run2_data -n 100 --data --no_exec --no_output --filein=$inputFile &> cmsDriver_l1Ntuple_RAW2DIGI.log
+## do not use --conditions=92X_dataRun2_HLT_XeXe_v1. It gives the exception with following message : No "RPCOMTFLinkMapRcd" record found in the EventSetup for synchronization value
+
+config_l1Ntuple="l1Ntuple_RAW2DIGI.py"
+config_l1Ntuple_FULL="${config_l1Ntuple/.py/_FULL.py}"
+edmConfigDump $config_l1Ntuple > $config_l1Ntuple_FULL
+
+cp $config_l1Ntuple_FULL "${config_l1Ntuple_FULL/.py/.BACKUP.py}"
+# replace "rawDataCollector" with "rawDataRepacker"
+sed -i "s/rawDataCollector/rawDataRepacker/g" $config_l1Ntuple_FULL
+
+echo "run the configs via"
+echo "myRun cmsRun $config_l1Ntuple &> ${config_l1Ntuple/.py/.log} &"
+echo "myRun cmsRun $config_l1Ntuple_FULL &> ${config_l1Ntuple_FULL/.py/.log} &"
+
