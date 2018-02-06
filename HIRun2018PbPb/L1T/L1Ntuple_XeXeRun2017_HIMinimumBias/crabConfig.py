589df4262d0768c2c5213f086652e0cadd42091f
Kaya Tatar <tatark@mit.edu>
2018-02-06 17:21:08 -0500

    update production with the new L1 workflow : l1t-integration-v97.7 branch modified for HI. no need to replace rawDataCollector with rawDataRepacker any more, hence no need to run the dumped config, but keep it for look-up purposes.

--- HIRun2018PbPb/L1T/L1Ntuple_XeXeRun2017_HIMinimumBias/crabConfig.py
+++ HIRun2018PbPb/L1T/L1Ntuple_XeXeRun2017_HIMinimumBias/crabConfig.py
@@ -8,22 +8,22 @@ config.General.transferLogs = False
 
 config.section_("JobType")
 config.JobType.pluginName = "Analysis"
-config.JobType.psetName = "l1Ntuple_RAW2DIGI_FULL.py"
+config.JobType.psetName = "l1Ntuple_RAW2DIGI.py"
 config.JobType.maxMemoryMB = 2500    # request high memory machines, 2500 is the maximum guaranteed number.
 config.JobType.maxJobRuntimeMin = 2750 # request longer runtime, ~47 hours. 2750 is the maximum guaranteed number.
 # CMSSW_9_4_0_pre3
-# L1 Stage2 instructions : https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuideL1TStage2Instructions?rev=123
-# Specific instructions for running on XeXeRun2017 data : https://twiki.cern.ch/twiki/bin/view/CMS/HiHighPtTrigger2018?rev=3#L1_Sample_Workflow
-# l1Ntuple_RAW2DIGI_FULL.py is created after calling ./createConfigs.sh
+# Instructions : https://twiki.cern.ch/twiki/bin/view/CMS/HiHighPtTrigger2018?rev=10#L1_Sample_Workflow
+# generic L1 Stage2 instructions : https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuideL1TStage2Instructions?rev=123
+# l1Ntuple_RAW2DIGI.py is created after calling ./createConfigs.sh
 
 config.section_("Data")
 config.Data.userInputFiles = open('inputFiles.txt').readlines()
 #config.Data.inputDBS = "phys03"
 config.Data.splitting = "FileBased"
-config.Data.unitsPerJob = 3
+config.Data.unitsPerJob = 10
 config.Data.totalUnits = -1
 
-config.Data.outputPrimaryDataset = "L1Trigger"
+config.Data.outputPrimaryDataset = "L1T"
 config.Data.outputDatasetTag = "L1Ntuple_XeXeRun2017_HIMinimumBias"
 config.Data.outLFNDirBase = "/store/user/katatar/HIRun2018PbPb/"
 
