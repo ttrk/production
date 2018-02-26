efc07a5d31e799020808a3ededbf48cfd8c8c0a3
Kaya Tatar <tatark@mit.edu>
2018-02-26 18:48:08 -0500

    update scripts with RECO step cmsDriver commands for Pythia8_Ze10e10 and Pythia8_Ze10e10_Hydjet_MB samples

--- EGamma/Pythia8_Ze10e10/createConfigs.sh
+++ EGamma/Pythia8_Ze10e10/createConfigs.sh
@@ -2,6 +2,14 @@
 
 runCmd="/afs/cern.ch/user/k/katatar/code/scripts/myRun.sh"
 
+set -x
+
 # https://twiki.cern.ch/twiki/bin/view/CMS/PP5TeVOfficialMC?rev=179#cmsDriver_Commands
 $runCmd cmsDriver.py step2 --pileup_input "dbs:/MinBias_TuneCUETP8M1_5p02TeV-pythia8/pp502Fall15-MCRUN2_71_V1-v1/GEN-SIM" --pileup_dasoption="--limit=0" --mc --eventcontent RAWSIM --pileup pp5TeV_Poisson_1p5 --customise SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1 --datatier GEN-SIM-RAW --conditions 75X_mcRun2_asymptotic_ppAt5TeV_v3 --step DIGI,L1,DIGI2RAW,HLT:PRef  -n 10 --no_exec --filein root://cms-xrd-global.cern.ch//store/himc/pp502Fall15/Pythia8_Ze10e10/GEN-SIM/MCRUN2_71_V1_ext1-v1/50000/0020CA3E-8C03-E611-B37D-44A842CFD5B1.root &> cmsDriver_step2_DIGI_L1_DIGI2RAW_HLT_PU.log
 
+$runCmd cmsDriver.py step3 --mc --eventcontent AODSIM --customise SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1 --datatier AODSIM --conditions 75X_mcRun2_asymptotic_ppAt5TeV_v3 --step RAW2DIGI,L1Reco,RECO -n 10 --no_exec --filein file:step2_DIGI_L1_DIGI2RAW_HLT_PU.root &> cmsDriver_step3_RAW2DIGI_L1Reco_RECO.log
+
+step3FileOrig="step3_RAW2DIGI_L1Reco_RECO.py"
+step3File="step3_RAW2DIGI_L1Reco_RECO_extendEC.py"
+mv $step3FileOrig $step3File
+cat step3ExtraLines.txt >> $step3File
--- EGamma/Pythia8_Ze10e10_Hydjet_MB/createConfigs.sh
+++ EGamma/Pythia8_Ze10e10_Hydjet_MB/createConfigs.sh
@@ -2,8 +2,16 @@
 
 runCmd="/afs/cern.ch/user/k/katatar/code/scripts/myRun.sh"
 
+set -x
+
 # https://twiki.cern.ch/twiki/bin/view/CMS/PbPb5TeVOfficialMC?rev=213#cmsDriver_Commands
 $runCmd cmsDriver.py step2 --pileup_input "dbs:/Hydjet_Quenched_MinBias_5020GeV_750/HiFall15-75X_mcRun2_HeavyIon_v1_75X_mcRun2_HeavyIon_v1-v1/GEN-SIM" --pileup_dasoption="--limit=0" --mc --eventcontent RAWSIM --pileup HiMix --customise SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1_HI --datatier GEN-SIM-RAW --conditions 75X_mcRun2_HeavyIon_v13 --step DIGI:pdigi_hi,L1,DIGI2RAW,HLT:HIon --scenario HeavyIons -n 10 --no_exec --filein root://cms-xrd-global.cern.ch//store/himc/HiFall15/Pythia8_Ze10e10_Hydjet_MB/GEN-SIM/75X_mcRun2_HeavyIon_v1_ext1-v1/60000/00B93BC6-7203-E611-A4FF-141877411970.root &> cmsDriver_step2_DIGI_L1_DIGI2RAW_HLT_PU.log
 
 # with command line options: step2 --pileup_input dbs:/Hydjet_Quenched_Cymbal5Ev8_PbPbMinBias_5020GeV/HiFall15-75X_mcRun2_HeavyIon_v14_75X_mcRun2_HeavyIon_v14-v1/GEN-SIM --pileup_dasoption=--limit=0 --mc --eventcontent RAWSIM --pileup HiMix --customise SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1_HI --datatier GEN-SIM-RAW --conditions 75X_mcRun2_HeavyIon_v14 --step DIGI:pdigi_hi,L1,DIGI2RAW,HLT:HIon --scenario HeavyIons -n 20 --no_exec --filein root://cms-xrd-global.cern.ch//store/himc/HiFall15/Pythia8_AllQCDPhoton30_Hydjet_Cymbal_MB/GEN-SIM/75X_mcRun2_HeavyIon_v14-v1/120000/005D04E2-57F4-E611-BAD4-02163E01A294.root
 
+$runCmd cmsDriver.py step3 --mc --eventcontent AODSIM --customise SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1_HI --datatier AODSIM --conditions 75X_mcRun2_HeavyIon_v13 --step RAW2DIGI,L1Reco,RECO --scenario HeavyIons -n 10 --no_exec --filein file:step2_DIGI_L1_DIGI2RAW_HLT_PU.root &> cmsDriver_step3_RAW2DIGI_L1Reco_RECO.log
+
+step3FileOrig="step3_RAW2DIGI_L1Reco_RECO.py"
+step3File="step3_RAW2DIGI_L1Reco_RECO_extendEC.py"
+mv $step3FileOrig $step3File
+cat step3ExtraLines.txt >> $step3File
