59a63f3f4cd6626396c584f976ffb803b463f688
Kaya Tatar <tatark@mit.edu>
2018-03-17 23:54:11 -0400

    add code block to check the output in a ROOT session

---
+++ HIRun2018PbPb/L1T/L1Ntuple_HIRun2015_HIMinimumBias2/checkTree.C
@@ -0,0 +1,7 @@
+l1UpgradeEmuTree->cd()
+L1UpgradeTree->GetEntries("egEt > 20 && abs(egEta) < 2.4")
+L1UpgradeTree->GetEntries("egEt > 15 && abs(egEta) < 2.4")
+L1UpgradeTree->GetEntries("egEt > 10 && abs(egEta) < 2.4")
+L1UpgradeTree->GetEntries("egEt > 5 && abs(egEta) < 2.4")
+L1UpgradeTree->GetEntries("egEt > 1 && abs(egEta) < 2.4")
+
