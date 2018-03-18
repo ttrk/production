717c3306577060d40314ce1fd1a3a9ef4aefac9f
Kaya Tatar <tatark@mit.edu>
2018-03-17 23:55:38 -0400

    update the way the TTree is read

--- HIRun2018PbPb/L1T/L1Ntuple_HIRun2015_HIMinimumBias2/checkTree.C
+++ HIRun2018PbPb/L1T/L1Ntuple_HIRun2015_HIMinimumBias2/checkTree.C
@@ -1,7 +1,7 @@
-l1UpgradeEmuTree->cd()
-L1UpgradeTree->GetEntries("egEt > 20 && abs(egEta) < 2.4")
-L1UpgradeTree->GetEntries("egEt > 15 && abs(egEta) < 2.4")
-L1UpgradeTree->GetEntries("egEt > 10 && abs(egEta) < 2.4")
-L1UpgradeTree->GetEntries("egEt > 5 && abs(egEta) < 2.4")
-L1UpgradeTree->GetEntries("egEt > 1 && abs(egEta) < 2.4")
+TTree* tree = (TTree*)gFile->Get("l1UpgradeEmuTree/L1UpgradeTree")
+tree->GetEntries("egEt > 20 && abs(egEta) < 2.4")
+tree->GetEntries("egEt > 15 && abs(egEta) < 2.4")
+tree->GetEntries("egEt > 10 && abs(egEta) < 2.4")
+tree->GetEntries("egEt > 5 && abs(egEta) < 2.4")
+tree->GetEntries("egEt > 1 && abs(egEta) < 2.4")
 
