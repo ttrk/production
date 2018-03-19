202f63ed54efeb7e5ccd541045d2ad05e872ce95
Kaya Tatar <tatark@mit.edu>
2018-03-19 10:18:09 -0400

    simple scripts to dump the provenance in pp RECO and hi RECO configurations.

---
+++ HIRun2018PbPb/Reco/timing/ppReco/runProvDump.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+runCmd="/afs/cern.ch/user/k/katatar/code/scripts/myRun.sh"
+
+inputFile="step3_RAW2DIGI_L1Reco_RECO_N100.root"
+outputFile="edmPD_"${inputFile/.root/.log}
+
+$runCmd edmProvDump $inputFile &> $outputFile
+
