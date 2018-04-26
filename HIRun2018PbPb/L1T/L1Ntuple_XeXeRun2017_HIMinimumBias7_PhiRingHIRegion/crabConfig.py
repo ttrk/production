712026f6ba04c04efea53e59219226f89c3b8edb
Kaya Tatar <tatark@mit.edu>
2018-04-25 23:52:16 -0400

    L1 emulation configs for different L1 jet algorithms. Sample is XeXeRun2017 HIMinimumBias7 data.

---
+++ HIRun2018PbPb/L1T/L1Ntuple_XeXeRun2017_HIMinimumBias7_PhiRingHIRegion/crabConfig.py
@@ -0,0 +1,35 @@
+from WMCore.Configuration import Configuration
+
+config = Configuration()
+
+config.section_("General")
+config.General.requestName = "L1Ntuple_XeXeRun2017_HIMinimumBias7_PhiRingHIRegion"
+config.General.transferLogs = False
+
+config.section_("JobType")
+config.JobType.pluginName = "Analysis"
+config.JobType.psetName = "l1Ntuple_RAW2DIGI.py"
+config.JobType.maxMemoryMB = 2500    # request high memory machines, 2500 is the maximum guaranteed number.
+config.JobType.maxJobRuntimeMin = 2750 # request longer runtime, ~47 hours. 2750 is the maximum guaranteed number.
+# CMSSW_10_0_0, l1t-integration-v97.17-v2
+# Instructions : https://twiki.cern.ch/twiki/bin/view/CMS/HiHighPtTrigger2018?rev=15#L1_Sample_Workflow
+# generic L1 Stage2 instructions : https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuideL1TStage2Instructions?rev=126#Environment_Setup_with_Integrati
+# l1Ntuple_RAW2DIGI.py is created after calling ./createConfigs.sh
+
+config.section_("Data")
+config.Data.userInputFiles = open('inputFiles.txt').readlines()
+#config.Data.inputDBS = "phys03"
+config.Data.splitting = "FileBased"
+config.Data.unitsPerJob = 10
+config.Data.totalUnits = -1
+
+config.Data.outputPrimaryDataset = "L1T"
+config.Data.outputDatasetTag = "L1Ntuple_XeXeRun2017_HIMinimumBias7_PhiRingHIRegion"
+config.Data.outLFNDirBase = "/store/user/katatar/HIRun2018PbPb/"
+
+config.section_("Site")
+config.Site.storageSite = "T2_US_MIT"
+config.Site.whitelist = ["T2_US_MIT"]
+
+config.section_("Debug")
+config.Debug.extraJDL = ["+CMS_ALLOW_OVERFLOW=False"]
---
+++ HIRun2018PbPb/L1T/L1Ntuple_XeXeRun2017_HIMinimumBias7_PhiRingPPTower/createConfigs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+runCmd="/afs/cern.ch/user/k/katatar/code/scripts/myRun.sh"
+
+inputFile="root://cms-xrd-global.cern.ch//store/hidata/XeXeRun2017/HIMinimumBias7/RAW/v1/000/304/899/00000/000AB151-8CAF-E711-A03A-02163E019BA4.root"
+
+# https://twiki.cern.ch/twiki/bin/view/CMS/L1HITaskForce?rev=38#Offline_SW_setup
+# https://twiki.cern.ch/twiki/bin/view/CMS/HiHighPtTrigger2018?rev=15#Running_L1Ntuple
+$runCmd cmsDriver.py l1Ntuple -s RAW2DIGI --era=Run2_2017_pp_on_XeXe --customise=L1Trigger/Configuration/customiseReEmul.L1TReEmulFromRAW --customise=L1Trigger/L1TNtuples/customiseL1Ntuple.L1NtupleEMU --customise=L1Trigger/Configuration/customiseUtils.L1TTurnOffUnpackStage2GtGmtAndCalo --customise=FWCore/ParameterSet/MassReplace.massReplaceInputTag --conditions=100X_dataRun2_v1 -n 100 --data --no_exec --no_output --filein=$inputFile &> cmsDriver_l1Ntuple_RAW2DIGI.log
+
+config_l1Ntuple="l1Ntuple_RAW2DIGI.py"
+echo "process.caloStage2Params.hiFlag = cms.uint32(1)" >> $config_l1Ntuple
+pusType="PhiRingPPTower"
+echo "process.caloStage2Params.jetPUSType = cms.string('$pusType')" >> $config_l1Ntuple
+
+config_l1Ntuple_LOG="${config_l1Ntuple/.py/.log}"
+echo "# run the config via"
+echo "myRun cmsRun $config_l1Ntuple &> $config_l1Ntuple_LOG &"
+
+## dump the full config
+config_l1Ntuple_FULL="${config_l1Ntuple/.py/_FULL.py}"
+edmConfigDump $config_l1Ntuple > $config_l1Ntuple_FULL
+echo "# Full config is also created : $config_l1Ntuple_FULL"
+
