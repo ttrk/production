5be297964433bc7f8268b145aeeef01b5cecad68
Kaya Tatar <tatark@mit.edu>
2018-08-03 18:18:05 -0400

    reco and foresting configurations for modified GED photon reco on SingleGammaFlatPt10To100, photon gun.
    The modified version (phiwidth SuperCluster = 0.20) is a narrower search window in phi direction for photon SuperClusters.

---
+++ EGamma/SingleGammaFlatPt10To100/RECO_pfSC_phiWidth0p20/crabConfig_FOREST.py
@@ -0,0 +1,41 @@
+from WMCore.Configuration import Configuration
+
+config = Configuration()
+
+config.section_("General")
+config.General.requestName = "SingleGammaFlatPt10To100_pythia8_FOREST_extendEC_pfSC_phiWidth0p20"
+config.General.transferLogs = False
+
+config.section_("JobType")
+config.JobType.pluginName = "Analysis"
+config.JobType.psetName = "runForestAOD_PbPb_MIX_75X_particleGun.py"
+config.JobType.maxMemoryMB = 2500    # request high memory machines.
+config.JobType.maxJobRuntimeMin = 2750    # request longer runtime, 48 hours.
+# forest_CMSSW_7_5_8_patch3
+# https://github.com/CmsHI/cmssw/commit/526e9291788c0b172f349f04bbdc37f472735f8b
+# runForestAOD_PbPb_MIX_75X.py commit
+# https://github.com/CmsHI/cmssw/commit/336108879815bf68a856f23a7a47254ef87347fd
+
+# runForestAOD_PbPb_MIX_75X_particleGun.py is a modified version of runForestAOD_PbPb_MIX_75X.py where
+# 1. process.ggHiNtuplizer.runOnParticleGun = cms.bool(True)
+#    process.ggHiNtuplizer.doRecHitsEB = cms.bool(True)   # see https://github.com/CmsHI/cmssw/commit/526e9291788c0b172f349f04bbdc37f472735f8b
+#    process.ggHiNtuplizer.doRecHitsEE = cms.bool(True)
+# 2. removing some Njettiness sequences
+
+config.section_("Data")
+config.Data.inputDataset = "/EGamma/katatar-SingleGammaFlatPt10To100_pythia8_RAW2DIGI_L1Reco_RECO_extendEC_pfSC_phiWidth0p20-ff1e2d3e968b7a0aa3af6d8376554fe5/USER"
+config.Data.inputDBS = "phys03"
+config.Data.splitting = "FileBased"
+config.Data.unitsPerJob = 1
+config.Data.totalUnits = -1
+config.Data.publication = False
+config.Data.outputDatasetTag = "SingleGammaFlatPt10To100_pythia8_FOREST_extendEC_pfSC_phiWidth0p20"
+config.Data.outLFNDirBase = "/store/user/katatar/"
+
+config.section_("Site")
+config.Site.storageSite = "T2_US_MIT"
+config.Site.whitelist = ["T2_US_MIT"]
+
+config.section_("Debug")
+config.Debug.extraJDL = ["+CMS_ALLOW_OVERFLOW=False"]
+
