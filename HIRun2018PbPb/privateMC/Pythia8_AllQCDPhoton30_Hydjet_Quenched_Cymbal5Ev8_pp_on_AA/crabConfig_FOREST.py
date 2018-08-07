bb3cdaeafe096b3ed122e38f22cd13a1f134e031
Kaya Tatar <tatark@mit.edu>
2018-08-07 17:26:16 -0400

    foresting configurations for private MC for PbPb 2018 run prep.
    instructions : https://twiki.cern.ch/twiki/bin/view/CMS/HiForestSetup?rev=52#Setup_for_10_1_0_pre2_For_PbPb_2
    software : forest_CMSSW_10_1_0_pre2

---
+++ HIRun2018PbPb/privateMC/Pythia8_AllQCDPhoton30_Hydjet_Quenched_Cymbal5Ev8_pp_on_AA/crabConfig_FOREST.py
@@ -0,0 +1,37 @@
+from WMCore.Configuration import Configuration
+
+config = Configuration()
+
+config.section_("General")
+config.General.requestName = "Pythia8_AllQCDPhoton30_Hydjet_Quenched_Cymbal5Ev8_pp_on_AA_FOREST"
+config.General.transferLogs = False
+
+config.section_("JobType")
+config.JobType.pluginName = "Analysis"
+config.JobType.psetName = "runForestAOD_PbPb_MC_101X.py"
+config.JobType.maxMemoryMB = 2500    # request high memory machines.
+config.JobType.maxJobRuntimeMin = 2750    # request longer runtime, 48 hours.
+# forest setup : https://twiki.cern.ch/twiki/bin/view/CMS/HiForestSetup?rev=52#Setup_for_10_1_0_pre2_For_PbPb_2
+# forest_CMSSW_10_1_0_pre2
+# https://github.com/bi-ran/cmssw/commit/f2808ca5cf8aac5e6d33d493cc6e1f1479132d3d
+# runForestAOD_PbPb_MC_101X.py commit
+# https://github.com/bi-ran/cmssw/commit/f2808ca5cf8aac5e6d33d493cc6e1f1479132d3d
+
+# Samples are listed here : https://twiki.cern.ch/twiki/bin/view/CMS/PbPb5TeV2018PrivateMC?rev=18#Tunes
+config.section_("Data")
+config.Data.inputDataset = "/Pythia8_AllQCDPhoton30_Hydjet_Quenched_Cymbal5Ev8/clindsey-AODSIM_20180723_pp_on_AAA-b7cacae03ea78f1a9a439e6efe1e6356/USER"
+config.Data.inputDBS = "phys03"
+config.Data.splitting = "FileBased"
+config.Data.unitsPerJob = 10
+config.Data.totalUnits = -1
+config.Data.publication = False
+config.Data.outputDatasetTag = "pp_on_AA_FOREST"
+config.Data.outLFNDirBase = "/store/user/katatar/HIRun2018PbPb/"
+
+config.section_("Site")
+config.Site.storageSite = "T2_US_MIT"
+config.Site.whitelist = ["T2_US_MIT"]
+
+config.section_("Debug")
+config.Debug.extraJDL = ["+CMS_ALLOW_OVERFLOW=False"]
+
---
+++ HIRun2018PbPb/privateMC/Pythia8_AllQCDPhoton30_Hydjet_Quenched_Cymbal5Ev8_pp_on_AA/runForestAOD_PbPb_MC_101X.py
@@ -0,0 +1,253 @@
+### HiForest Configuration
+# Collisions: pp
+# Type: MC
+# Input: AOD
+
+import FWCore.ParameterSet.Config as cms
+process = cms.Process('HiForest')
+process.options = cms.untracked.PSet()
+
+#####################################################################################
+# HiForest labelling info
+#####################################################################################
+
+process.load("HeavyIonsAnalysis.JetAnalysis.HiForest_cff")
+process.HiForest.inputLines = cms.vstring("HiForest V3",)
+import subprocess
+version = subprocess.Popen(["(cd $CMSSW_BASE/src && git describe --tags)"], stdout=subprocess.PIPE, shell=True).stdout.read()
+if version == '':
+    version = 'no git info'
+process.HiForest.HiForestVersion = cms.string(version)
+
+#####################################################################################
+# Input source
+#####################################################################################
+
+process.source = cms.Source("PoolSource",
+    duplicateCheckMode = cms.untracked.string("noDuplicateCheck"),
+    fileNames = cms.untracked.vstring(
+        '/store/user/clindsey/Pythia8_AllQCDPhoton30_Hydjet_Quenched_Cymbal5Ev8/AODSIM_20180723_pp_on_AAA/180724_062627/0000/step3_RAW2DIGI_L1Reco_RECO_100.root'
+    )
+)
+
+# Number of events we want to process, -1 = all events
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(10))
+
+
+#####################################################################################
+# Load Global Tag, Geometry, etc.
+#####################################################################################
+
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('Configuration.Geometry.GeometryDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+
+from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '100X_upgrade2018_realistic_v10', '')
+process.HiForest.GlobalTagLabel = process.GlobalTag.globaltag
+process.GlobalTag.snapshotTime = cms.string("9999-12-31 23:59:59.000")
+process.GlobalTag.toGet.extend([
+   cms.PSet(record = cms.string("HeavyIonRcd"),
+      tag = cms.string("CentralityTable_HFtowers200_HydjetCymbal5Ev8_v758x03_mc"),
+      connect = cms.string("frontier://FrontierProd/CMS_CONDITIONS"),
+      label = cms.untracked.string("HFtowersCymbal5Ev8")
+   ),
+])
+
+process.load('RecoHI.HiCentralityAlgos.HiCentrality_cfi')
+
+process.load("RecoHI.HiCentralityAlgos.CentralityBin_cfi")
+process.centralityBin.Centrality = cms.InputTag("hiCentrality")
+process.centralityBin.centralityVariable = cms.string("HFtowers")
+process.centralityBin.nonDefaultGlauberModel = cms.string("Cymbal5Ev8")
+
+# Customization
+#from HeavyIonsAnalysis.Configuration.CommonFunctions_cff import overrideJEC_pp5020
+#process = overrideJEC_pp5020(process)
+
+#####################################################################################
+# Define tree output
+#####################################################################################
+
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string("HiForestAOD.root"))
+
+#####################################################################################
+# Additional Reconstruction and Analysis: Main Body
+#####################################################################################
+
+####################################################################################
+
+#############################
+# Jets
+#############################
+
+process.load("HeavyIonsAnalysis.JetAnalysis.FullJetSequence_XeXe_mc")
+process.akPu4PFcorr.payload = "AK4PF"
+process.akCs4PFcorr.payload = "AK4PF"
+process.ak4PFcorr.payload = "AK4PF"
+# Use this version for JEC
+#process.load("HeavyIonsAnalysis.JetAnalysis.FullJetSequence_JECPP")
+
+#####################################################################################
+
+############################
+# Event Analysis
+############################
+process.load('HeavyIonsAnalysis.EventAnalysis.hltanalysis_cff')
+process.load('HeavyIonsAnalysis.EventAnalysis.hievtanalyzer_mc_cfi')
+process.hiEvtAnalyzer.Vertex = cms.InputTag("offlinePrimaryVertices")
+process.hiEvtAnalyzer.doCentrality = cms.bool(True)
+process.hiEvtAnalyzer.CentralitySrc = cms.InputTag("hiCentrality")
+process.hiEvtAnalyzer.CentralityBinSrc = cms.InputTag("centralityBin","HFtowers")
+process.hiEvtAnalyzer.doEvtPlane = cms.bool(False)
+process.hiEvtAnalyzer.doMC = cms.bool(True) #general MC info
+process.hiEvtAnalyzer.doHiMC = cms.bool(True) #HI specific MC info
+
+process.load('HeavyIonsAnalysis.JetAnalysis.HiGenAnalyzer_cfi')
+process.HiGenParticleAna.genParticleSrc = cms.untracked.InputTag("genParticles")
+process.HiGenParticleAna.doHI = False
+process.load('HeavyIonsAnalysis.EventAnalysis.runanalyzer_cff')
+
+process.load("HeavyIonsAnalysis.JetAnalysis.pfcandAnalyzer_cfi")
+process.pfcandAnalyzer.skipCharged = False
+process.pfcandAnalyzer.pfPtMin = 0
+process.pfcandAnalyzer.pfCandidateLabel = cms.InputTag("particleFlow")
+process.pfcandAnalyzer.doVS = cms.untracked.bool(False)
+process.pfcandAnalyzer.doUEraw_ = cms.untracked.bool(False)
+process.pfcandAnalyzer.genLabel = cms.InputTag("genParticles")
+process.pfcandAnalyzerCS = process.pfcandAnalyzer.clone(
+    pfCandidateLabel = cms.InputTag("akCs4PFJets","pfParticlesCs"))
+
+#####################################################################################
+
+#########################
+# Track Analyzer
+#########################
+process.load('HeavyIonsAnalysis.JetAnalysis.ExtraTrackReco_cff')
+process.load('HeavyIonsAnalysis.JetAnalysis.TrkAnalyzers_cff')
+
+# Use this instead for track corrections
+##process.load('HeavyIonsAnalysis.JetAnalysis.TrkAnalyzers_Corr_cff')
+
+#####################################################################################
+
+#####################
+# photons
+######################
+process.load('HeavyIonsAnalysis.PhotonAnalysis.ggHiNtuplizer_cfi')
+process.ggHiNtuplizer.gsfElectronLabel = cms.InputTag("gedGsfElectrons")
+process.ggHiNtuplizer.recoPhotonSrc = cms.InputTag("islandPhotons")
+process.ggHiNtuplizer.recoPhotonHiIsolationMap = cms.InputTag('photonIsolationHIProducerppIsland')
+process.ggHiNtuplizer.useValMapIso = cms.bool(True)
+process.ggHiNtuplizer.VtxLabel = cms.InputTag("offlinePrimaryVertices")
+process.ggHiNtuplizer.particleFlowCollection = cms.InputTag("particleFlow")
+process.ggHiNtuplizer.doVsIso = cms.bool(False)
+process.ggHiNtuplizer.doElectronVID = cms.bool(True)
+process.ggHiNtuplizerGED = process.ggHiNtuplizer.clone(
+    recoPhotonSrc = cms.InputTag('gedPhotons'),
+    recoPhotonHiIsolationMap = cms.InputTag('photonIsolationHIProducerppGED'))
+
+####################################################################################
+#####################
+# Electron ID
+#####################
+
+# from PhysicsTools.SelectorUtils.tools.vid_id_tools import *
+# turn on VID producer, indicate data format to be processed
+# DataFormat.AOD or DataFormat.MiniAOD
+# dataFormat = DataFormat.AOD
+# switchOnVIDElectronIdProducer(process, dataFormat)
+
+# define which IDs we want to produce. Check here https://twiki.cern.ch/twiki/bin/viewauth/CMS/CutBasedElectronIdentificationRun2#Recipe_for_regular_users_for_7_4
+# my_id_modules = ['RecoEgamma.ElectronIdentification.Identification.cutBasedElectronID_Spring15_25ns_V1_cff']
+
+#add them to the VID producer
+# for idmod in my_id_modules:
+#     setupAllVIDIdsInModule(process,idmod,setupVIDElectronSelection)
+#####################################################################################
+
+#########################
+# Main analysis list
+#########################
+process.ana_step = cms.Path(
+    # process.hltanalysisReco *
+    # process.hiCentrality *
+    # process.centralityBin *
+    # process.hiEvtAnalyzer *
+    process.HiGenParticleAna *
+    process.jetSequences +
+    # process.egmGsfElectronIDSequence + #Should be added in the path for VID module
+    process.ggHiNtuplizer +
+    process.ggHiNtuplizerGED +
+    process.pfcandAnalyzer +
+    process.pfcandAnalyzerCS +
+    process.HiForest +
+    process.trackSequencesPP +
+    process.runAnalyzer
+)
+
+#####################################################################################
+
+#########################
+# Event Selection
+#########################
+
+process.load('HeavyIonsAnalysis.JetAnalysis.EventSelection_cff')
+# process.pHBHENoiseFilterResultProducer = cms.Path(process.HBHENoiseFilterResultProducer)
+# process.HBHENoiseFilterResult = cms.Path(process.fHBHENoiseFilterResult)
+# process.HBHENoiseFilterResultRun1 = cms.Path(process.fHBHENoiseFilterResultRun1)
+# process.HBHENoiseFilterResultRun2Loose = cms.Path(process.fHBHENoiseFilterResultRun2Loose)
+# process.HBHENoiseFilterResultRun2Tight = cms.Path(process.fHBHENoiseFilterResultRun2Tight)
+# process.HBHEIsoNoiseFilterResult = cms.Path(process.fHBHEIsoNoiseFilterResult)
+
+process.PAprimaryVertexFilter = cms.EDFilter("VertexSelector",
+    src = cms.InputTag("offlinePrimaryVertices"),
+    cut = cms.string("!isFake && abs(z) <= 25 && position.Rho <= 2 && tracksSize >= 2"),
+    filter = cms.bool(True), # otherwise it won't filter the events
+)
+
+process.NoScraping = cms.EDFilter("FilterOutScraping",
+    applyfilter = cms.untracked.bool(True),
+    debugOn = cms.untracked.bool(False),
+    numtrack = cms.untracked.uint32(10),
+    thresh = cms.untracked.double(0.25)
+)
+
+process.pPAprimaryVertexFilter = cms.Path(process.PAprimaryVertexFilter)
+process.pBeamScrapingFilter=cms.Path(process.NoScraping)
+
+process.load("HeavyIonsAnalysis.VertexAnalysis.PAPileUpVertexFilter_cff")
+
+process.pVertexFilterCutG = cms.Path(process.pileupVertexFilterCutG)
+process.pVertexFilterCutGloose = cms.Path(process.pileupVertexFilterCutGloose)
+process.pVertexFilterCutGtight = cms.Path(process.pileupVertexFilterCutGtight)
+process.pVertexFilterCutGplus = cms.Path(process.pileupVertexFilterCutGplus)
+process.pVertexFilterCutE = cms.Path(process.pileupVertexFilterCutE)
+process.pVertexFilterCutEandG = cms.Path(process.pileupVertexFilterCutEandG)
+
+process.load('HeavyIonsAnalysis.Configuration.hfCoincFilter_cff')
+process.phfCoincFilter1 = cms.Path(process.hfCoincFilter)
+process.phfCoincFilter2 = cms.Path(process.hfCoincFilter2)
+process.phfCoincFilter3 = cms.Path(process.hfCoincFilter3)
+process.phfCoincFilter4 = cms.Path(process.hfCoincFilter4)
+process.phfCoincFilter5 = cms.Path(process.hfCoincFilter5)
+
+process.pAna = cms.EndPath(process.skimanalysis)
+
+# Customization
+process.akPu4PFJetAnalyzer.doExtendedFlavorTagging = cms.untracked.bool(False)
+process.akPu4PFJetAnalyzer.trackSelection = process.ak4PFSecondaryVertexTagInfos.trackSelection
+process.akPu4PFJetAnalyzer.trackPairV0Filter = process.ak4PFSecondaryVertexTagInfos.vertexCuts.v0Filter
+process.akCs4PFJetAnalyzer.doExtendedFlavorTagging = cms.untracked.bool(False)
+process.akCs4PFJetAnalyzer.trackSelection = process.ak4PFSecondaryVertexTagInfos.trackSelection
+process.akCs4PFJetAnalyzer.trackPairV0Filter = process.ak4PFSecondaryVertexTagInfos.vertexCuts.v0Filter
+process.ak4CaloJetAnalyzer.doExtendedFlavorTagging = cms.untracked.bool(False)
+process.ak4CaloJetAnalyzer.trackSelection = process.ak4PFSecondaryVertexTagInfos.trackSelection
+process.ak4CaloJetAnalyzer.trackPairV0Filter = process.ak4PFSecondaryVertexTagInfos.vertexCuts.v0Filter
+process.ak4PFJetAnalyzer.doExtendedFlavorTagging = cms.untracked.bool(False)
+process.ak4PFJetAnalyzer.trackSelection = process.ak4PFSecondaryVertexTagInfos.trackSelection
+process.ak4PFJetAnalyzer.trackPairV0Filter = process.ak4PFSecondaryVertexTagInfos.vertexCuts.v0Filter
