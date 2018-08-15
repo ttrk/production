bd41137afb0ac34e2242b2ca0509ebdfc8f02fec
Kaya Tatar <tatark@mit.edu>
2018-08-14 23:53:32 -0400

    configurations to run L1Ntuple for EGs on private MC for PbPb 2018 run prep. l1Ntuple configs are created by https://github.com/ttrk/production/blob/master/HIRun2018PbPb/L1T/L1Ntuple_Pythia8_AllQCDPhoton15_Hydjet_Quenched_Cymbal5Ev8/createConfigs.sh

---
+++ HIRun2018PbPb/L1T/L1Ntuple_Pythia8_EmEnrichedDijet30_Hydjet_Quenched_Cymbal5Ev8/l1Ntuple_RAW2DIGI.py
@@ -0,0 +1,105 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: l1Ntuple -s RAW2DIGI --era=Run2_2018 --customise=L1Trigger/Configuration/customiseReEmul.L1TReEmulMCFromRAW --customise=L1Trigger/L1TNtuples/customiseL1Ntuple.L1NtupleEMU --customise=L1Trigger/L1TNtuples/customiseL1Ntuple.L1NtupleGEN --customise=L1Trigger/Configuration/customiseUtils.L1TTurnOffUnpackStage2GtGmtAndCalo --customise=L1Trigger/Configuration/customiseSettings.L1TSettingsToCaloParams_2018_v1_2 --customise_commands=process.simTwinMuxDigis.DTDigi_Source = cms.InputTag('bmtfDigis')\n process.simTwinMuxDigis.DTThetaDigi_Source = cms.InputTag('bmtfDigis')\n process.simTwinMuxDigis.RPC_Source = cms.InputTag('muonRPCDigis')\n --conditions=101X_upgrade2018_realistic_v7 -n 100 --mc --no_exec --no_output --filein=root://xrootd.cmsaf.mit.edu//store/user/clindsey/Pythia8_AllQCDPhoton15_Hydjet_Quenched_Cymbal5Ev8/RAWSIM_20180630/180630_163544/0000/step1_DIGI_L1_DIGI2RAW_HLT_PU_863.root
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.StandardSequences.Eras import eras
+
+process = cms.Process('RAW2DIGI',eras.Run2_2018)
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('Configuration.StandardSequences.RawToDigi_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(100)
+)
+
+# Input source
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('root://xrootd.cmsaf.mit.edu//store/user/clindsey/Pythia8_AllQCDPhoton15_Hydjet_Quenched_Cymbal5Ev8/RAWSIM_20180630/180630_163544/0000/step1_DIGI_L1_DIGI2RAW_HLT_PU_863.root'),
+    secondaryFileNames = cms.untracked.vstring()
+)
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('l1Ntuple nevts:100'),
+    name = cms.untracked.string('Applications'),
+    version = cms.untracked.string('$Revision: 1.19 $')
+)
+
+# Output definition
+
+# Additional output definition
+
+# Other statements
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '101X_upgrade2018_realistic_v7', '')
+
+# Path and EndPath definitions
+process.raw2digi_step = cms.Path(process.RawToDigi)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.raw2digi_step,process.endjob_step)
+from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
+associatePatAlgosToolsTask(process)
+
+# customisation of the process.
+
+# Automatic addition of the customisation function from L1Trigger.Configuration.customiseReEmul
+from L1Trigger.Configuration.customiseReEmul import L1TReEmulMCFromRAW 
+
+#call to customisation function L1TReEmulMCFromRAW imported from L1Trigger.Configuration.customiseReEmul
+process = L1TReEmulMCFromRAW(process)
+
+# Automatic addition of the customisation function from L1Trigger.L1TNtuples.customiseL1Ntuple
+from L1Trigger.L1TNtuples.customiseL1Ntuple import L1NtupleEMU,L1NtupleGEN 
+
+#call to customisation function L1NtupleEMU imported from L1Trigger.L1TNtuples.customiseL1Ntuple
+process = L1NtupleEMU(process)
+
+#call to customisation function L1NtupleGEN imported from L1Trigger.L1TNtuples.customiseL1Ntuple
+process = L1NtupleGEN(process)
+
+# Automatic addition of the customisation function from L1Trigger.Configuration.customiseUtils
+from L1Trigger.Configuration.customiseUtils import L1TTurnOffUnpackStage2GtGmtAndCalo 
+
+#call to customisation function L1TTurnOffUnpackStage2GtGmtAndCalo imported from L1Trigger.Configuration.customiseUtils
+process = L1TTurnOffUnpackStage2GtGmtAndCalo(process)
+
+# Automatic addition of the customisation function from L1Trigger.Configuration.customiseSettings
+from L1Trigger.Configuration.customiseSettings import L1TSettingsToCaloParams_2018_v1_2 
+
+#call to customisation function L1TSettingsToCaloParams_2018_v1_2 imported from L1Trigger.Configuration.customiseSettings
+process = L1TSettingsToCaloParams_2018_v1_2(process)
+
+# End of customisation functions
+
+# Customisation from command line
+
+process.simTwinMuxDigis.DTDigi_Source
+# Add early deletion of temporary data products to reduce peak memory need
+from Configuration.StandardSequences.earlyDeleteSettings_cff import customiseEarlyDelete
+process = customiseEarlyDelete(process)
+# End adding early deletion
+process.caloStage2Params.egBypassExtHOverE = cms.uint32(1)
+process.caloStage2Params.egBypassShape = cms.uint32(1)
+process.caloStage2Params.egBypassECALFG = cms.uint32(1)
+process.caloStage2Params.egHOverEcutBarrel = cms.int32(1)
+process.caloStage2Params.egHOverEcutEndcap = cms.int32(1)
+process.caloStage2Params.egEtaCut = cms.int32(24)
---
+++ HIRun2018PbPb/L1T/L1Ntuple_Pythia8_EmEnrichedDijet50_Hydjet_Quenched_Cymbal5Ev8/crabConfig.py
@@ -0,0 +1,38 @@
+from WMCore.Configuration import Configuration
+
+config = Configuration()
+
+config.section_("General")
+config.General.requestName = "L1Ntuple_Pythia8_EmEnrichedDijet50_Hydjet_Quenched_Cymbal5Ev8"
+config.General.transferLogs = False
+
+config.section_("JobType")
+config.JobType.pluginName = "Analysis"
+config.JobType.psetName = "l1Ntuple_RAW2DIGI.py"
+config.JobType.maxMemoryMB = 2500    # request high memory machines, 2500 is the maximum guaranteed number.
+config.JobType.maxJobRuntimeMin = 2750 # request longer runtime, ~47 hours. 2750 is the maximum guaranteed number.
+## Software
+# CMSSW_10_2_1, l1t-integration-v99.0
+# This integration contains the new L1 EG bypass flags : https://github.com/cms-l1t-offline/cmssw/pull/708
+# https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuideL1TStage2Instructions?rev=140#Environment_Setup_with_Integrati
+## Driver
+# https://twiki.cern.ch/twiki/bin/view/CMS/L1HITaskForce?rev=42#Offline_SW_setup
+# l1Ntuple_RAW2DIGI.py is created after calling ./createConfigs.sh
+
+# Samples are listed here : https://twiki.cern.ch/twiki/bin/view/CMS/PbPb5TeV2018PrivateMC?rev=19#Tunes
+config.section_("Data")
+config.Data.inputDataset = "/Pythia8_EmEnrichedDijet50_Hydjet_Quenched_Cymbal5Ev8/clindsey-RAWSIM_20180630-d863108fee469c130ddd2763f36829bb/USER"
+config.Data.inputDBS = "phys03"
+config.Data.splitting = "FileBased"
+config.Data.unitsPerJob = 120
+config.Data.totalUnits = -1
+config.Data.publication = False
+config.Data.outputDatasetTag = "L1Ntuple_egHOverEcut_EB1_EE1_egEtaCut24"
+config.Data.outLFNDirBase = "/store/user/katatar/HIRun2018PbPb/L1T/"
+
+config.section_("Site")
+config.Site.storageSite = "T2_US_MIT"
+config.Site.whitelist = ["T2_US_MIT"]
+
+config.section_("Debug")
+config.Debug.extraJDL = ["+CMS_ALLOW_OVERFLOW=False"]
