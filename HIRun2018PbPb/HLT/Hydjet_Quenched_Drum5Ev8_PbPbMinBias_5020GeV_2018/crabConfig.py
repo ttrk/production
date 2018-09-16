3e482789ddc407c5ad034eb2e0c947ee3fe18b5f
Kaya Tatar <tatark@mit.edu>
2018-09-16 19:50:30 -0400

    Dataset is now valid and at T2_US_MIT. Process full sample with menu hltTestEgamma_V34.

--- HIRun2018PbPb/HLT/Hydjet_Quenched_Drum5Ev8_PbPbMinBias_5020GeV_2018/crabConfig.py
+++ HIRun2018PbPb/HLT/Hydjet_Quenched_Drum5Ev8_PbPbMinBias_5020GeV_2018/crabConfig.py
@@ -3,7 +3,7 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = "Hydjet_Quenched_Drum5Ev8_PbPbMinBias_5020GeV_2018_hltTestEgamma_V32"
+config.General.requestName = "Hydjet_Quenched_Drum5Ev8_PbPbMinBias_5020GeV_2018_hltTestEgamma_V34"
 config.General.transferLogs = False
 
 config.section_("JobType")
@@ -17,20 +17,20 @@ config.JobType.maxJobRuntimeMin = 2750    # request longer runtime, ~48 hours.
 
 # Samples are listed here : https://twiki.cern.ch/twiki/bin/view/CMS/PbPb5TeV2018OfficialMC?rev=15#Available_Samples_AN1
 config.section_("Data")
-config.Data.allowNonValidInputDataset = True    # because the dataset is not 'VALID' but 'PRODUCTION'.
+#config.Data.allowNonValidInputDataset = True    # because the dataset is not 'VALID' but 'PRODUCTION'.
 config.Data.inputDataset = "/Hydjet_Quenched_Drum5Ev8_PbPbMinBias_5020GeV_2018/HINPbPbSpring18DR-NoPU_100X_upgrade2018_realistic_v10_ext1-v1/GEN-SIM-RAW"
 config.Data.inputDBS = "global"
 config.Data.splitting = "FileBased"
-config.Data.unitsPerJob = 10
+config.Data.unitsPerJob = 20
 config.Data.totalUnits = -1
 config.Data.publication = False
-config.Data.outputDatasetTag = "hltTestEgamma_V32"
+config.Data.outputDatasetTag = "hltTestEgamma_V34"
 config.Data.outLFNDirBase = "/store/user/katatar/HIRun2018PbPb/HLT/"
 
 config.section_("Site")
 config.Site.storageSite = "T2_US_MIT"
-#config.Site.whitelist = ["T2_US_MIT"]
+config.Site.whitelist = ["T2_US_MIT"]
 
-#config.section_("Debug")
-#config.Debug.extraJDL = ["+CMS_ALLOW_OVERFLOW=False"]
+config.section_("Debug")
+config.Debug.extraJDL = ["+CMS_ALLOW_OVERFLOW=False"]
 
