1ab08237958e2611ef84a0890ace5ab05e80cbbb
Kaya Tatar <tatark@mit.edu>
2018-09-16 19:48:12 -0400

    process full sample with menu hltTestEgamma_V34

--- HIRun2018PbPb/HLT/Pythia8_AllQCDPhoton15_Hydjet_Quenched_Cymbal5Ev8/crabConfig.py
+++ HIRun2018PbPb/HLT/Pythia8_AllQCDPhoton15_Hydjet_Quenched_Cymbal5Ev8/crabConfig.py
@@ -3,7 +3,7 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = "Pythia8_AllQCDPhoton15_Hydjet_Quenched_Cymbal5Ev8_hltTestEgamma_V32"
+config.General.requestName = "Pythia8_AllQCDPhoton15_Hydjet_Quenched_Cymbal5Ev8_hltTestEgamma_V34"
 config.General.transferLogs = False
 
 config.section_("JobType")
@@ -23,7 +23,7 @@ config.Data.splitting = "FileBased"
 config.Data.unitsPerJob = 30
 config.Data.totalUnits = -1
 config.Data.publication = False
-config.Data.outputDatasetTag = "hltTestEgamma_V32"
+config.Data.outputDatasetTag = "hltTestEgamma_V34"
 config.Data.outLFNDirBase = "/store/user/katatar/HIRun2018PbPb/HLT/"
 
 config.section_("Site")
