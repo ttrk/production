da3f1517db92a250f47e9b3695be746910ce0b49
Kaya Tatar <tatark@mit.edu>
2018-10-29 00:11:11 +0100

    reco scripts, configurations and crab configs for embedded AllQCDPhoton15 and EmEnrichedDijet30 samples. Follows the recipe for 103X validation : https://twiki.cern.ch/twiki/bin/view/CMS/HiHighPtRecoValidation2018?rev=83#CMSSW_10_3_0_pre5_DIGI_Recipes_f . Input DIGI dataset is the output of https://github.com/ttrk/production/commit/bed78bae20e72648a97dac62f0fd45ce0236ee3e

---
+++ HIRun2018PbPb/privateMC/Pythia8_EmEnrichedDijet30_Hydjet_Quenched_Cymbal5Ev8_pp_on_AA/crabConfig_RAW2DIGI_L1Reco_RECO.py
@@ -0,0 +1,31 @@
+from WMCore.Configuration import Configuration
+
+config = Configuration()
+
+config.section_("General")
+config.General.requestName = "Pythia8_EmEnrichedDijet30_Hydjet_Quenched_Cymbal5Ev8_103X_RAW2DIGI_L1Reco_RECO"
+config.General.transferLogs = False
+
+config.section_("JobType")
+config.JobType.pluginName = "Analysis"
+config.JobType.psetName = "step3_RAW2DIGI_L1Reco_RECO.py"
+config.JobType.maxMemoryMB = 2500    # request high memory machines.
+config.JobType.maxJobRuntimeMin = 2750    # request longer runtime, ~48 hours.
+
+## software : CMSSW_10_3_0_pre5
+
+config.section_("Data")
+config.Data.inputDataset = "/Pythia8_EmEnrichedDijet30_Hydjet_Quenched_Cymbal5Ev8/katatar-Pythia8_EmEnrichedDijet30_Hydjet_Quenched_Cymbal5Ev8_DIGI_L1_DIGI2RAW_HLT_PU-c269713c668aa4d7b49bd0c3c5d5dcad/USER"
+config.Data.inputDBS = "phys03"
+config.Data.splitting = "FileBased"
+config.Data.unitsPerJob = 2
+config.Data.totalUnits = -1
+config.Data.outputDatasetTag = "103X_RAW2DIGI_L1Reco_RECO"
+config.Data.outLFNDirBase = "/store/user/katatar/HIRun2018PbPb/"
+
+config.section_("Site")
+config.Site.storageSite = "T2_US_MIT"
+#config.Site.whitelist = ["T2_US_MIT"]
+
+#config.section_("Debug")
+#config.Debug.extraJDL = ["+CMS_ALLOW_OVERFLOW=False"]
