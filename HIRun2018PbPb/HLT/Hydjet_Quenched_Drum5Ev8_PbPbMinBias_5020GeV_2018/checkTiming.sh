2ced01b79636d09d642d052d092e401bbba4077b
Kaya Tatar <tatark@mit.edu>
2018-11-12 20:11:32 +0100

    check timing for different HLT_HIGEDPhoton20 paths

--- HIRun2018PbPb/HLT/Hydjet_Quenched_Drum5Ev8_PbPbMinBias_5020GeV_2018/checkTiming.sh
+++ HIRun2018PbPb/HLT/Hydjet_Quenched_Drum5Ev8_PbPbMinBias_5020GeV_2018/checkTiming.sh
@@ -14,3 +14,31 @@ grep --color -i "   HLT_HIGEDPhoton30_EB_L1EG15_v1" $inputFile
 echo "#####"
 grep --color -i "   HLT_HIGEDPhoton30_EB_L1Seeded_v1" $inputFile
 echo "#####"
+
+grep --color -i "   HLT_HIGEDPhoton20_v1" $inputFile
+echo "#####"
+grep --color -i "   HLT_HIGEDPhoton20_L1EG7_v1" $inputFile
+echo "#####"
+grep --color -i "   HLT_HIGEDPhoton20_L1Seeded_v1" $inputFile
+echo "#####"
+grep --color -i "   HLT_HIGEDPhoton20_EB_v1" $inputFile
+echo "#####"
+grep --color -i "   HLT_HIGEDPhoton20_EB_L1EG7_v1" $inputFile
+echo "#####"
+grep --color -i "   HLT_HIGEDPhoton20_EB_L1Seeded_v1" $inputFile
+echo "#####"
+
+
+#FastReport       289.1 ms HLT_HIGEDPhoton30_v1
+######
+#FastReport       176.8 ms HLT_HIGEDPhoton30_L1EG15_v1
+######
+#FastReport       174.7 ms HLT_HIGEDPhoton30_L1Seeded_v1
+######
+#FastReport       289.1 ms HLT_HIGEDPhoton30_EB_v1
+######
+#FastReport       176.8 ms HLT_HIGEDPhoton30_EB_L1EG15_v1
+######
+#FastReport       174.7 ms HLT_HIGEDPhoton30_EB_L1Seeded_v1
+######
+
