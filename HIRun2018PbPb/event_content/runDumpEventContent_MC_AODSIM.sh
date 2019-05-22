2a7792e42d4d17694c289ed3a7737557c77578e2
Kaya Tatar <tatark@mit.edu>
2019-05-22 15:23:29 -0400

    simple scripts to dump event content of AOD for PbPb 2018

---
+++ HIRun2018PbPb/event_content/runDumpEventContent_MC_AODSIM.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#runCmd="/afs/cern.ch/user/k/katatar/code/scripts/myRun.sh"
+runCmd=""
+
+inputFile="root://xrootd-cms.infn.it//store/himc/HINPbPbAutumn18DR/QCDPhoton_pThat-15_TuneCP5_PbPb_5p02TeV_pythia8/AODSIM/NoPU_103X_upgrade2018_realistic_HI_v11-v1/90000/F034BBC1-62CB-3C4D-BFDA-8B2529C79A62.root"
+outputFile="edmDEC_HIRun2018PbPb_MC_AODSIM.log"
+
+$runCmd edmDumpEventContent --all $inputFile &> $outputFile
+
