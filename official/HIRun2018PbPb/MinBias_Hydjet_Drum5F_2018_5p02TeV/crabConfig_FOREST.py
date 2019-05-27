1a617f52c68292182e5b2484827d59c63406797e
Kaya Tatar <tatark@mit.edu>
2019-05-27 16:00:36 -0400

    foresting config for official Hydjet MB sample for 2018 PbPb

---
+++ official/HIRun2018PbPb/MinBias_Hydjet_Drum5F_2018_5p02TeV/crabConfig_FOREST.py
@@ -0,0 +1,36 @@
+from WMCore.Configuration import Configuration
+
+config = Configuration()
+
+config.section_("General")
+config.General.requestName = "MinBias_Hydjet_Drum5F_2018_5p02TeV_HINPbPbAutumn18DR-NoPU-FOREST"
+config.General.transferLogs = False
+
+config.section_("JobType")
+config.JobType.pluginName = "Analysis"
+config.JobType.psetName = "runForestAOD_pponAA_MB_103X.py"
+config.JobType.maxMemoryMB = 2500    # request high memory machines.
+config.JobType.maxJobRuntimeMin = 2750    # request longer runtime.
+# forest_CMSSW_10_3_1
+# https://github.com/CmsHI/cmssw/commit/ba8faf19f2c60adc8318182a386eb8ffa3dea9b2
+## runForestAOD_pponAA_MIX_103X.py commit + enable ggHiNtuplizerGED.doEffectiveAreas + enable ggHiNtuplizerGED.doRecHits + enable l1object
+# https://github.com/CmsHI/cmssw/commit/a7368e9ee00017993af087b6265f97023d98c14b
+# relevant DAS query : summary dataset=/MinBias_Hydjet_Drum5F_2018_5p02TeV/HINPbPbAutumn18DR-NoPU_103X_upgrade2018_realistic_HI_v11-v1/AODSIM
+#   output on 2019.05.24 : Number of blocks: 8 Number of events: 999111 Number of files: 248 Number of lumis: 7868 sum(file_size): 1060981783565 (1.1TB)
+
+config.section_("Data")
+config.Data.inputDataset = "/MinBias_Hydjet_Drum5F_2018_5p02TeV/HINPbPbAutumn18DR-NoPU_103X_upgrade2018_realistic_HI_v11-v1/AODSIM"
+config.Data.inputDBS = "global"
+config.Data.splitting = "FileBased"
+config.Data.unitsPerJob = 1
+config.Data.totalUnits = -1
+config.Data.publication = False
+config.Data.outputDatasetTag = "HINPbPbAutumn18DR-NoPU_103X_upgrade2018_realistic_HI_v11-v1-FOREST"
+config.Data.outLFNDirBase = "/store/user/katatar/official/HIRun2018PbPb/"
+
+config.section_("Site")
+config.Site.storageSite = "T2_US_MIT"
+config.Site.whitelist = ["T2_US_MIT"]
+
+config.section_("Debug")
+config.Debug.extraJDL = ["+CMS_ALLOW_OVERFLOW=False"]
