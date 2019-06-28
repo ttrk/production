1625d2bbe130674e0d6a1bd443b1f276c84069c4
Kaya Tatar <tatark@mit.edu>
2019-06-28 13:48:50 -0400

    foresting config for PbPb 2018 HIMinimumBias1 dataset split into run ranges

---
+++ data/HIRun2018PbPb/HIMinimumBias1_HIRun2018A_04Apr2019_v1/crabConfig_FOREST_runRange_326381_326800.py
@@ -0,0 +1,38 @@
+from WMCore.Configuration import Configuration
+
+config = Configuration()
+
+config.section_("General")
+config.General.requestName = "HIMinimumBias1_HIRun2018A_04Apr2019_v1_Run326381_326800_FOREST"
+config.General.transferLogs = False
+
+config.section_("JobType")
+config.JobType.pluginName = "Analysis"
+config.JobType.psetName = "runForestAOD_pponAA_DATA_103X.py"
+config.JobType.maxMemoryMB = 2500    # request high memory machines.
+config.JobType.maxJobRuntimeMin = 2750    # request longer runtime.
+# forest_CMSSW_10_3_1
+# https://github.com/CmsHI/cmssw/commit/419b0eb45d6fc64bf427dcc668c46ebf2aa33921
+# runForestAOD_pponAA_DATA_103X.py commit + ggHi.doEffectiveAreas + enable ggHiNtuplizerGED doRecHits and doPhoERegression + activate l1object + HLT photon filter
+# https://github.com/CmsHI/cmssw/commit/a7368e9ee00017993af087b6265f97023d98c14b
+# relevant DAS query : summary dataset=/HIMinimumBias1/HIRun2018A-04Apr2019-v1/AOD
+#   output on 2019.06.18 : Number of blocks: 265 Number of events: 217067743 Number of files: 35184 Number of lumis: 41293 sum(file_size): 103672587054982 (103.7TB)
+
+config.section_("Data")
+config.Data.inputDataset = "/HIMinimumBias1/HIRun2018A-04Apr2019-v1/AOD"
+config.Data.inputDBS = "global"
+config.Data.lumiMask = "Cert_326381-327564_HI_PromptReco_Collisions18_JSON.txt" # /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions18/HI/PromptReco/Cert_326381-327564_HI_PromptReco_Collisions18_JSON.txt
+config.Data.runRange = "326381-326800"
+config.Data.splitting = "FileBased"
+config.Data.unitsPerJob = 2
+config.Data.totalUnits = -1
+config.Data.publication = False
+config.Data.outputDatasetTag = "HIRun2018A-04Apr2019-v1-Run326381-326800-FOREST"
+config.Data.outLFNDirBase = "/store/user/katatar/HIRun2018PbPb/"
+
+config.section_("Site")
+config.Site.storageSite = "T2_US_MIT"
+#config.Site.whitelist = ["T2_US_Vanderbilt"]
+
+#config.section_("Debug")
+#config.Debug.extraJDL = ["+CMS_ALLOW_OVERFLOW=False"]
