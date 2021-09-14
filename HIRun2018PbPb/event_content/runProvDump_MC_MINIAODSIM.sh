8971ff58a07d300a6d6c289d187f33f9f3dd4426
Kaya Tatar <kaya.tatar@cern.ch>
2021-09-14 15:21:46 +0200

    edm Dump files for MINIAODSIM

---
+++ HIRun2018PbPb/event_content/runProvDump_MC_MINIAODSIM.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#runCmd="/afs/cern.ch/user/k/katatar/code/scripts/myRun.sh"
+runCmd=""
+
+inputFile="root://xrootd-cms.infn.it//store/himc/HINPbPbSpring21MiniAOD/QCDPhoton_pThat-15_TuneCP5_caloParmStage2v3_HydjetDrumMB_5p02TeV_Pythia8/MINIAODSIM/FixL1CaloGT_112X_upgrade2018_realistic_HI_v9-v1/230000/04db870e-af38-481c-a2c7-0a52fb290868.root"
+outputFile="edmPD_HIRun2018PbPb_MC_MINIAODSIM_HINPbPbSpring21MiniAOD.log"
+
+$runCmd edmProvDump $inputFile &> $outputFile
+
