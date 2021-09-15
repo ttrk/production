cc2f932c25d653ae4ae39916a733aa5b58afbfcd
Kaya Tatar <kaya.tatar@cern.ch>
2021-09-15 18:15:22 +0200

    edm Dump files for PbPb 2018 data MINIAOD

---
+++ HIRun2018PbPb/event_content/runDumpEventContent_DATA_MINIAOD.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#runCmd="/afs/cern.ch/user/k/katatar/code/scripts/myRun.sh"
+runCmd=""
+
+inputFile="root://xrootd-cms.infn.it//store/hidata/HIRun2018A/HIHardProbes/MINIAOD/PbPb18_MiniAODv1-v1/00000/034807c1-0ae5-4540-bb81-80ab2f3bc01d.root"
+outputFile="edmDEC_HIRun2018PbPb_DATA_MINIAOD.log"
+
+$runCmd edmDumpEventContent --all $inputFile &> $outputFile
+
