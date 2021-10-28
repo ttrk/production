3b2689e83fdf7c1b2ee1aad612959eed49f8ac01
Kaya Tatar <kaya.tatar@cern.ch>
2021-10-28 19:51:32 +0200

    edm dump files for PbPb 2018 data RAW

---
+++ HIRun2018PbPb/event_content/runProvDump_DATA_RAW.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#runCmd="/afs/cern.ch/user/k/katatar/code/scripts/myRun.sh"
+runCmd=""
+
+inputFile="root://xrootd.cmsaf.mit.edu//store/hidata/HIRun2018A/HIMinimumBias2/RAW/v1/000/327/402/00000/BF49D3F3-CA0C-264E-8384-3D2BECD313EB.root"
+outputFile="edmPD_HIRun2018PbPb_DATA_RAW.log"
+
+$runCmd edmProvDump $inputFile &> $outputFile
+
