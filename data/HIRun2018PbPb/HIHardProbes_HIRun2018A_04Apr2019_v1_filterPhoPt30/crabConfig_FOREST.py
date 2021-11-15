0b8dd3d5000833fb9216fd73bb0689b353b711f9
Kaya Tatar <kaya.tatar@cern.ch>
2021-11-15 13:30:59 +0100

    foresting HIHardProbes filtered for photons with pt > 30

---
+++ data/HIRun2018PbPb/HIHardProbes_HIRun2018A_04Apr2019_v1_filterPhoPt30/crabConfig_FOREST.py
@@ -0,0 +1,40 @@
+from WMCore.Configuration import Configuration
+
+config = Configuration()
+
+config.section_("General")
+config.General.requestName = "HIHardProbes_HIRun2018A_04Apr2019_v1_Cert_326381_327564_filterPhoPt30_FOREST"
+config.General.transferLogs = False
+
+config.section_("JobType")
+config.JobType.pluginName = "Analysis"
+config.JobType.psetName = "runForestAOD_pponAA_DATA_103X.py"
+config.JobType.maxMemoryMB = 2500    # request high memory machines.
+#config.JobType.maxJobRuntimeMin = 2750    # request longer runtime.
+## software : CMSSW_10_3_1
+## forest_CMSSW_10_3_1
+# https://github.com/CmsHI/cmssw/commit/5c5ccb4013e2ebecfbca0ca433cb14bb36b9f1d3
+# runForestAOD_pponAA_DATA_103X.py commit + ggHi.doEffectiveAreas + enable ggHiNtuplizerGED doRecHits and doPhoERegression + activate l1object + add event plane info + photon filter
+# https://github.com/CmsHI/cmssw/commit/4d7967c0beee30ae2573fa9108ae9925d620c9e5
+# relevant DAS query : summary dataset=/HIHardProbes/HIRun2018A-04Apr2019-v1/AOD
+#   output on 2019.06.17 : Number of blocks: 108 Number of events: 43654157 Number of files: 7747 Number of lumis: 41396 sum(file_size): 74107362626557 (74.1TB)
+config.JobType.inputFiles = ["HeavyIonRPRcd_PbPb2018_offline.db"]
+
+config.section_("Data")
+config.Data.inputDataset = "/HIHardProbes/HIRun2018A-04Apr2019-v1/AOD"
+config.Data.inputDBS = "global"
+config.Data.lumiMask = "Cert_326381-327564_HI_PromptReco_Collisions18_JSON.txt" # /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions18/HI/PromptReco/Cert_326381-327564_HI_PromptReco_Collisions18_JSON.txt
+#config.Data.runRange = "326500-326886"
+config.Data.splitting = "FileBased"
+config.Data.unitsPerJob = 1
+config.Data.totalUnits = 3000
+config.Data.publication = False
+config.Data.outputDatasetTag = "HIRun2018A-04Apr2019-v1-Cert-326381-327564-filterPhoPt30-FOREST"
+config.Data.outLFNDirBase = "/store/user/katatar/HIRun2018PbPb/"
+
+config.section_("Site")
+config.Site.storageSite = "T2_US_MIT"
+config.Site.whitelist = ["T2_US_Vanderbilt"]
+
+config.section_("Debug")
+config.Debug.extraJDL = ["+CMS_ALLOW_OVERFLOW=False"]
